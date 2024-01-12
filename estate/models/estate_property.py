5621b912f27b001795307696f9865e6666ee6b02
AnutToc <anuttocharoenchok@gmail.com>
2024-01-12 19:31:30 +0700

    Chapter 7: Basic Views

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,22 +1,28 @@
 # -*- coding: utf-8 -*-
 from odoo import fields, models
 
+from dateutil.relativedelta import relativedelta
+
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate"
 
-    name = fields.Char(required=True)
+    def _default_date_availability(self):
+        return fields.Datetime.today(self) + relativedelta(months=3)
+
+    name = fields.Char(required=True, string="Title")
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=lambda self: self._default_date_availability())
     expected_price = fields.Float(required=True)
-    selling_price = fields.Integer()
-    bedrooms = fields.Integer()
+    selling_price = fields.Integer(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    # garden_orientation = fields.Selection()
-
+    garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')], default='north')
+    active = fields.Boolean(default=False)
+    state = fields.Selection([('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')], required=True, copy=False, default='new')
 
