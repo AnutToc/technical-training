8a8214af435fc0b18ace4ea54bb1573ebff13437
AnutToc <anuttocharoenchok@gmail.com>
2024-01-16 17:28:14 +0700

    Chapter 10: Ready For Some Action?

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models
+from odoo import fields, models, api, _
 
 from dateutil.relativedelta import relativedelta
-
+from odoo.exceptions import UserError
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate"
@@ -15,14 +15,14 @@ class EstateProperty(models.Model):
     postcode = fields.Char()
     date_availability = fields.Date(copy=False, default=lambda self: self._default_date_availability())
     expected_price = fields.Float(required=True)
-    selling_price = fields.Integer(readonly=True, copy=False)
+    selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')], default='north')
+    garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
     active = fields.Boolean(default=False)
     state = fields.Selection([('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')], required=True, copy=False, default='new')
 
@@ -31,3 +31,52 @@ class EstateProperty(models.Model):
     buyer_id = fields.Many2one("res.partner", string="Buyer", readonly=True, copy=False)
     tag_ids = fields.Many2many("estate.property.tag", string="Tags")
     offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
+
+    total_area = fields.Float(copy=False, string='Total Area (sqm)', compute='_compute_total_area')
+    best_price = fields.Float(copy=False, string='Best Offer', compute='_compute_best_price')
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends('offer_ids.price')
+    def _compute_best_price(self):
+        for record in self:
+            if record.offer_ids:
+                record.best_price = max(record.offer_ids.mapped("price"))
+            else:
+                record.best_price = 0.0
+
+    @api.onchange("garden")
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = False
+
+    def action_sold(self):
+        if self.state == 'canceled':
+            raise UserError(_("Canceled properties cannot be sold."))
+        else:
+            self.state = 'sold'
+
+    # Recommend This Way
+    # def action_sold(self):
+    #     if "canceled" in self.mapped("state"):
+    #         raise UserError("Canceled properties cannot be sold.")
+    #     return self.write({"state": "sold"})
+
+    def action_cancel(self):
+        if self.state == 'sold':
+            raise UserError(_("Sold properties cannot be canceled."))
+        else:
+            self.state = 'canceled'
+
+    # Recommend This Way
+    # def action_cancel(self):
+    #     if "sold" in self.mapped("state"):
+    #         raise UserError("Sold properties cannot be canceled.")
+    #     return self.write({"state": "canceled"})
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models
+from odoo import fields, models, api, _
+
+from dateutil.relativedelta import relativedelta
+from odoo.exceptions import UserError
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -8,3 +11,61 @@ class EstatePropertyOffer(models.Model):
     status = fields.Selection([('accepted','Accepted'), ('refused', 'Refused')], copy=False)
     partner_id = fields.Many2one("res.partner", required=True)
     property_id= fields.Many2one("estate.property", required=True)
+
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute='_compute_date_deadline', inverse='_inverse_date_deadline')
+
+    @api.depends('create_date', 'validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                date = record.create_date.date()
+            else:
+                date = fields.Date.today()
+
+            record.date_deadline = date + relativedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                date = record.create_date.date()
+            else:
+                date = fields.Date.today()
+            
+            record.validity = (record.date_deadline - date).days
+
+    def action_accepted(self):
+        if "accepted" in self.mapped("property_id.offer_ids.status"):
+            raise UserError(_("An offer has already been accepted."))
+        else:
+            self.status = 'accepted'
+            self.property_id.selling_price = self.price
+            self.property_id.buyer_id = self.partner_id
+
+    # Recommend This Way
+    # def action_accept(self):
+    #     if "accepted" in self.mapped("property_id.offer_ids.state"):
+    #         raise UserError("An offer as already been accepted.")
+    #     self.write(
+    #         {
+    #             "state": "accepted",
+    #         }
+    #     )
+    #     return self.mapped("property_id").write(
+    #         {
+    #             "state": "offer_accepted",
+    #             "selling_price": self.price,
+    #             "buyer_id": self.partner_id.id,
+    #         }
+    #     )
+
+    def action_refused(self):
+        self.status = 'refused'
+
+    # Recommend This Way
+    # def action_refuse(self):
+    #     return self.write(
+    #         {
+    #             "state": "refused",
+    #         }
+    #     )
